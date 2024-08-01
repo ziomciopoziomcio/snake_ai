8fe3b1de3481d1d2f331a220852ee12f55aec64f
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2024-08-01 14:48:32 +0200

    Game engine

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ board_width = 20
 board_height = 20
 snake_speed = 5
 amount_of_food = 1
+snake_amount = 1
 
 
 # game objects
@@ -23,5 +24,11 @@ class Snake:
         self.score = 0
         self.color = (222, 78, 55)
 
-
-
+class Game:
+    def __init__(self):
+        self.snake_amount = snake_amount
+        self.snakes = [Snake() for _ in range(self.snake_amount)]
+        self.food_amount = amount_of_food
+        self.food = [snake_helper.random_position(board_height, board_width) for _ in range(self.food_amount)]
+        self.game_over = False
+        
