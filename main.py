9d99b2c6783d74959edb06ccfa20246012676654
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2024-08-07 11:03:01 +0200

    added pygame window

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ amount_of_food = 1
 snake_amount = 1
 
 
+
+
 # game objects
 
 class Snake:
@@ -24,6 +26,8 @@ class Snake:
         self.score = 0
         self.color = (222, 78, 55)
 
+
+
 class Game:
     def __init__(self):
         self.snake_amount = snake_amount
@@ -31,4 +35,23 @@ class Game:
         self.food_amount = amount_of_food
         self.food = [snake_helper.random_position(board_height, board_width) for _ in range(self.food_amount)]
         self.game_over = False
-        
+
+# pygame setup
+
+pygame.init()
+screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption('Snake')
+clock = pygame.time.Clock()
+game = Game()
+running = True
+
+while running:
+    screen.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    # update display
+    pygame.display.update()
+    clock.tick(snake_speed)
