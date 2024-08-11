fe8ab0b11cf61f5f80e5f8180e57e3557b1426a5
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2024-08-11 19:45:05 +0200

    #3 fix

--- components/snake_helper.py
+++ components/snake_helper.py
@@ -1,7 +1,7 @@
 import random
 
 def random_position(height, width):
-    return [random.randint(0, height - 1), random.randint(0, width - 1)]
+    return [random.randint(1, height - 2), random.randint(1, width - 2)]
 
 def random_direction():
     return random.choice(['UP', 'DOWN', 'LEFT', 'RIGHT'])
