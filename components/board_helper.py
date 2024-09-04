cbcce7ac2eed1e78027314c09cd542a3ecac765a
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2024-09-04 23:11:49 +0200

    changed border logic

--- components/board_helper.py
+++ components/board_helper.py
@@ -2,11 +2,7 @@ import pygame
 
 
 def draw_border(screen, color, board_width, board_height, window_width, window_height):
-    cell_size = min(window_width // board_width, window_height // board_height)
-    thickness = cell_size
-    pygame.draw.rect(screen, color, pygame.Rect(0, 0, board_width * cell_size, thickness))  # Top border
-    pygame.draw.rect(screen, color, pygame.Rect(0, 0, thickness, board_height * cell_size))  # Left border
-    pygame.draw.rect(screen, color, pygame.Rect(0, (board_height - 1) * cell_size, board_width * cell_size,
-                                                thickness))  # Bottom border
-    pygame.draw.rect(screen, color,
-                     pygame.Rect((board_width - 1) * cell_size, 0, thickness, board_height * cell_size))  # Right border
+    cell_size = min(window_width / board_width, window_height / board_height)
+    border_rect = pygame.Rect(0, 0, board_width * cell_size, board_height * cell_size)
+    border_rect.center = (window_width // 2, window_height // 2)
+    pygame.draw.rect(screen, color, border_rect, 1)
