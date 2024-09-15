3d41a7bcf3978eb6695ea0150783877d5a1331ef
nina <ninazugaj25@gmail.com>
2024-09-15 12:12:40 +0200

    snakes spawning on each other and too close to each other fixed

--- components/snake_helper.py
+++ components/snake_helper.py
@@ -1,8 +1,14 @@
 import random
 
 
-def random_position(height, width):
-    return [random.randint(1, height - 2), random.randint(1, width - 2)]
+def random_position(height, width, existing_positions=None, min_distance=3):
+    if existing_positions is None:
+        existing_positions = []
+
+    while True:
+        position = [random.randint(1, height - 2), random.randint(1, width - 2)]
+        if all([abs(position[0] - existing_position[0]) >= min_distance or abs(position[1] - existing_position[1]) >= min_distance for existing_position in existing_positions]):
+            return position
 
 
 def random_direction(directions):
