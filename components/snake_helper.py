406b5e7d88122d04455f44338a5c6f52a0268f61
nina <ninazugaj25@gmail.com>
2024-09-15 12:29:55 +0200

    Update snake_helper.py

--- components/snake_helper.py
+++ components/snake_helper.py
@@ -7,7 +7,8 @@ def random_position(height, width, existing_positions=None, min_distance=3):
 
     while True:
         position = [random.randint(1, height - 2), random.randint(1, width - 2)]
-        if all([abs(position[0] - existing_position[0]) >= min_distance or abs(position[1] - existing_position[1]) >= min_distance for existing_position in existing_positions]):
+        if all([abs(position[0] - existing_position[0]) >= min_distance or abs(
+                position[1] - existing_position[1]) >= min_distance for existing_position in existing_positions]):
             return position
 
 
