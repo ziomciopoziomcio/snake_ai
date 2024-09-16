5b0ecd1e1cee201a22ef1654daa44b9f5db8bde6
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2024-09-16 20:07:52 +0200

    Fix

--- components/board_helper.py
+++ components/board_helper.py
@@ -5,4 +5,4 @@ def draw_border(screen, color, board_width, board_height, window_width, window_h
     cell_size = min(window_width / board_width, window_height / board_height)
     border_rect = pygame.Rect(0, 0, board_width * cell_size, board_height * cell_size)
     border_rect.center = (window_width // 2, window_height // 2)
-    pygame.draw.rect(screen, color, border_rect, 1)
+    pygame.draw.rect(screen, color, border_rect, int(cell_size))
