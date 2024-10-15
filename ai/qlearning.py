31e74f36fece369a1f3e8dd25dfd1f387856598f
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2024-10-15 14:03:27 +0200

    now it actually makes sense
    Co-Authored-By: Nina Zugaj <154976830+ninsson@users.noreply.github.com>

--- ai/qlearning.py
+++ ai/qlearning.py
@@ -2,27 +2,25 @@ import numpy as np
 import random
 from main import Game, ai_move
 
-head_position = None
-points = None
-
-
-def update_points(p):
-    global points
-    points = p
-
-def update_head_position(position):
-    global head_position
-    head_position = position
 
 class SnakeEnv:
     def __init__(self):
         self.game = Game()
-        self.reset()
+        self.qvalues = {}
+        self.learning_rate = 0.1
+        self.discount = 0.9
 
-    def reset(self):
-        self.game = Game()
-        self.done = False
-        return self._get_state()
+    def update(self, state, action, next_state, reward):
+        if state not in self.qvalues:
+            self.qvalues[state] = np.zeros(4)
+        if next_state not in self.qvalues:
+            self.qvalues[next_state] = np.zeros(4)
+        self.qvalues[state][action] = self.qvalues[state][action] + self.learning_rate * (reward + self.discount * max(self.qvalues[next_state]) - self.qvalues[state][action])
 
-
+    def get_action(self, state):
+        if state not in self.qvalues:
+            self.qvalues[state] = np.zeros(4)
+        value = max(self.qvalues[state])
+        index = random.choice([i for i, v in enumerate(self.qvalues[state]) if v == value])
+        return index
 
