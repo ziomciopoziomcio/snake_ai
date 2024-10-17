d7c04daf1ba9c1560a078ab9b07c20f9343d482a
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2024-10-17 14:48:38 +0200

    Added learning_agent

--- ai/learning_agent.py
+++ ai/learning_agent.py
@@ -1,3 +1,16 @@
 from main import run
 
+board_width = 20
+board_height = 20
+snake_speed = 8
+amount_of_food = 1
+snake_amount = 1
+score_type = 1
+game_mode = 5
+window_width = 800
+window_height = 800
+turned_on = False
 
+for i in range(100):
+    run(board_width, board_height, snake_speed, amount_of_food, snake_amount, window_width, window_height, score_type,
+        game_mode)
