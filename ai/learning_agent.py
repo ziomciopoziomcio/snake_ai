663960a7f17bd8c45e3aa24fad38e93dd0551e79
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2024-10-17 15:14:39 +0200

    learning parameters changed

--- ai/learning_agent.py
+++ ai/learning_agent.py
@@ -2,7 +2,7 @@ from main import run
 
 board_width = 20
 board_height = 20
-snake_speed = 8
+snake_speed = 25
 amount_of_food = 1
 snake_amount = 1
 score_type = 1
@@ -11,6 +11,6 @@ window_width = 800
 window_height = 800
 turned_on = False
 
-for i in range(100):
+for i in range(1002):
     run(board_width, board_height, snake_speed, amount_of_food, snake_amount, window_width, window_height, score_type,
         game_mode)
