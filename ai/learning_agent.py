7358fd1f87adda337aff5c55d87721d991415f3c
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2024-10-17 18:55:44 +0200

    Some agent parameters changed

--- ai/learning_agent.py
+++ ai/learning_agent.py
@@ -3,7 +3,7 @@ import time
 
 board_width = 20
 board_height = 20
-snake_speed = 100000
+snake_speed = 10000
 amount_of_food = 1
 snake_amount = 1
 score_type = 1
@@ -13,9 +13,11 @@ window_height = 800
 turned_on = False
 
 for j in range(10):
-    print("Waiting 100")
-    time.sleep(100)
+    if j != 0:
+        print("Waiting 100")
+        time.sleep(100)
     for z in range(10):
+        print("Waiting 10")
         time.sleep(10)
         for i in range(100):
             run(board_width, board_height, snake_speed, amount_of_food, snake_amount, window_width, window_height, score_type,
