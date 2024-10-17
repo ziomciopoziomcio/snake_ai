49bef44934a5518a0e6fe36dd3d9058be16da0a4
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2024-10-17 16:27:55 +0200

    efficiency improvements

--- ai/learning_agent.py
+++ ai/learning_agent.py
@@ -1,8 +1,9 @@
 from main import run
+import time
 
 board_width = 20
 board_height = 20
-snake_speed = 25
+snake_speed = 100000
 amount_of_food = 1
 snake_amount = 1
 score_type = 1
@@ -11,6 +12,11 @@ window_width = 800
 window_height = 800
 turned_on = False
 
-for i in range(1000):
-    run(board_width, board_height, snake_speed, amount_of_food, snake_amount, window_width, window_height, score_type,
-        game_mode)
+for j in range(10):
+    print("Waiting 100")
+    time.sleep(100)
+    for z in range(10):
+        time.sleep(10)
+        for i in range(100):
+            run(board_width, board_height, snake_speed, amount_of_food, snake_amount, window_width, window_height, score_type,
+                game_mode)
