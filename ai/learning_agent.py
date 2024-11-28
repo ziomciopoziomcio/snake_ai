16d173077e0350fc9a06b7bc6bc9029afb8777f5
nina <ninazugaj25@gmail.com>
2024-11-28 07:41:18 +0100

    Update learning_agent.py

--- ai/learning_agent.py
+++ ai/learning_agent.py
@@ -115,7 +115,7 @@ if acceleration_mode == "y":
     counter = counter_glob
 else:
     for j in range(1):
-        for z in range(100):
+        for z in range(300):
             for i in range(10):
                 qnetwork, counter = run(board_width, board_height, snake_speed, amount_of_food, snake_amount, window_width,
                                         window_height,
